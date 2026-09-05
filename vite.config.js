9b381137e7f89f4920bbc2e7da9a16d70521553a
usman.shahid <usman.shahid@grp.contegris.com>
2026-09-05 17:02:55 +0500

    bhuild

--- vite.config.js
+++ vite.config.js
@@ -4,8 +4,17 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+
   server: {
     port: 5173,
     host: true
+  },
+
+  preview: {
+    host: true,
+    port: 5173,
+    allowedHosts: [
+      '.trycloudflare.com'
+    ]
   }
 })
